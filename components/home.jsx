57eae2a80fe73c0529520f2546023ddd1a4b6e68
xXA2005 <A2005BALILA@gmail.com>
2023-03-15 19:59:34 +0300

    a

--- components/home.jsx
+++ components/home.jsx
@@ -5,7 +5,7 @@ const Home = () => (
     <div className="gradient-01 min-w-full min-h-screen absolute top-0 left-0 z-[-1]" />
     <div className="">
       <h1 className="text-5xl font-bold py-3">hi im <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-900 to-red-500 font-extrabold">Ab.5</span></h1>
-      <h className="font-bold">epik Abdullah</h>
+      <h className="font-bold grad_text">epik Abdullah</h>
     </div>
   </div>
 );
