e00066c3e933cc1bfeb383c6c7d2fd3a0c151de2
xXA2005 <A2005BALILA@gmail.com>
2023-05-24 16:45:52 +0300

    contacts updated cuz ab5 term

--- components/contact.jsx
+++ components/contact.jsx
@@ -4,9 +4,9 @@ const btn = 'border p-3 rounded-md cursor-pointer glow-blue text-blue-800';
 
 const Contact = () => (
   <div className='min-h-screen flex justify-center items-center gap-4' id='contact'>
-    <a className={btn} href='https://discord.com/users/661693178960936970'>Discord Ab.5#9111</a>
+    <a className={btn} href='https://discordlookup.com/user/1105503842763800597'>Discord</a>
     <a className={btn} href='mailto:ab5@terrorist.services'>Email</a>
-    <a className={btn} href='https://t.me/OxAB5'>Telegram</a>
+    <a className={btn} href='https://t.me/Ab5fr'>Telegram</a>
     <a className={btn} href='https://github.com/xXA2005'>Github</a>
   </div>
 );
